62ab694e08935ffeecac19227d32682d32926a06
WTYLER9 <wtyler9889@gmail.com>
2024-07-07 00:09:24 -0400

    Both Data Files Are Read In And Merged

--- 2024 Data Analysis Capstone Code.py
+++ 2024 Data Analysis Capstone Code.py
@@ -1,8 +1,23 @@
 import math
-import numpy
-import pandas
+import numpy as np
+import pandas as pd
 import sklearn
 
-pd = pandas.read_csv("Keeneland Race Data Since Oct-2006.csv")
+df = pd.read_csv("Keeneland Race Data Since Oct-2006.csv")
+df = df[df["RaceMeet"] == "Spring 2024"]
+df['Index'] = np.arange(len(df))
+print(df.head())
+print(len(df))
 
-print(pd.head())
+df_main = pd.read_csv("Keeneland April 2024 Data for Capstone Project.csv")
+print(df_main.isnull().values.any())
+winners = df_main[df_main["Finish"] == "1"]
+winners['Index'] = np.arange(len(winners))
+print(df_main.head(20))
+print(winners.head(15))
+print(len(winners))
+
+df_merged = pd.merge(left=winners,right=df,on='Index')
+print(df_merged.head())
+
+df_merged.to_csv("merged_data_sets.csv")
