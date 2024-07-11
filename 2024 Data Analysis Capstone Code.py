19a7d91f84d7d9b1292a1d7a2a489bcc9106a985
WTYLER9 <wtyler9889@gmail.com>
2024-07-11 00:09:05 -0400

    Condensed merged data set, started summary stats

--- 2024 Data Analysis Capstone Code.py
+++ 2024 Data Analysis Capstone Code.py
@@ -18,6 +18,33 @@ print(winners.head(15))
 print(len(winners))
 
 df_merged = pd.merge(left=winners,right=df,on='Index')
+df_merged = df_merged[['Day','Weekday','Race Number','Post Position','M/L Odds','Final Odds','Distance','FieldSize',
+                       'Favorite','HalfMileTime','FinalTime','WinnersTrainer','WinnersJockey','ExactaPayout',
+                       'SuperfectaPayout','TrifectaPayout']]
 print(df_merged.head())
+df_merged['Final Odds'] = df_merged['Final Odds'].astype('Float64')
+print(df_merged.convert_dtypes().dtypes)
 
-df_merged.to_csv("merged_data_sets.csv")
+#df_merged.to_csv("merged_data_sets.csv")
+
+winning_post_positions = df_merged['Post Position'].value_counts()
+print(winning_post_positions)
+winning_jockey = df_merged['WinnersJockey'].value_counts()
+print(winning_jockey)
+winning_trainer = df_merged['WinnersTrainer'].value_counts()
+print(winning_trainer)
+number_of_scratches = len(df_main[df_main["Finish"]=='S'])
+print(number_of_scratches)
+#use merged
+#average_field_size_by_weekday = 
+#average_halfmile_by_distance = 
+#average_finaltime_by_distance =
+#average_mlodds = 
+#average_finalodds = 
+
+#use main
+#calculate change in odds
+#M/L odds model
+#Final Odds model
+#change in odds model
+#compare models
